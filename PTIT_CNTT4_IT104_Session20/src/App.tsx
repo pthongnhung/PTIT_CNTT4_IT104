d0f21488331e789d44daf668bc603fee3edbe1a2
pthongnhung <pthongnhung13122006@gmail.com>
2025-09-03 08:15:00 +0700

    add

--- PTIT_CNTT4_IT104_Session20/src/App.tsx
+++ PTIT_CNTT4_IT104_Session20/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+// import Welcome from './components/PTIT_CNTT4_IT104_Session20_ Exercises03/Welcome'
+// import PageTitle from './components/PTIT_CNTT4_IT104_Session20_ Exercises04/PageTitle'
+// import InputLengthCheck from './components/PTIT_CNTT4_IT104_Session20_ Exercises01/InputLengthCheck'
+// import Timer from './components/PTIT_CNTT4_IT104_Session20_ Exercises05/Timer'
+// import ModalFocus from './components/PTIT_CNTT4_IT104_Session20_ Exercises06/ModalFocus'
+// import Counter from './components/PTIT_CNTT4_IT104_Session20_ Exercises07/Counter'
+import UserForm from './components/PTIT_CNTT4_IT104_Session20_ Exercises08/UserForm'
+// import UserProfile from './components/PTIT_CNTT4_IT104_Session20_ Exercises02/UserProfile'
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* <Welcome></Welcome>
+      <hr />
+      <PageTitle></PageTitle>
+      <hr />
+      <InputLengthCheck></InputLengthCheck>
+      <hr />
+      <Timer></Timer>
+      <hr />
+      <ModalFocus></ModalFocus>
+      <hr />
+      <Counter></Counter>
+      <hr /> */}
+      <UserForm></UserForm>
+      <hr />
+      {/* <UserProfile></UserProfile> */}
+    </div>
   )
 }
-
-export default App
