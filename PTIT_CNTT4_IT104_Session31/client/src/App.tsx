07f7b00b4331473b198e9c5c322ab751a47fe6fa
pthongnhung <pthongnhung13122006@gmail.com>
2025-09-17 21:03:47 +0700

    add

---
+++ PTIT_CNTT4_IT104_Session31/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostManager from './pages/PostManager'
+
+export default function App() {
+  return (
+    <div>
+      <PostManager></PostManager>
+    </div>
+  )
+}
+
+
